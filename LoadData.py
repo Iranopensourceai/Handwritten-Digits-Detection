7cf8614363b16007cae26622417a4b79c8cd8565
Amirmahdi <alavi_amirmahdi@yahoo.com>
2023-01-23 03:04:32 +0330

    added test dataset

--- LoadData.py
+++ LoadData.py
@@ -3,16 +3,23 @@ from scipy import ndimage
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+from skimage.transform import resize
+from sklearn.neighbors import KNeighborsClassifier
+# import cv2
+
 
 class LoadDigits:
-    def __init__(self, mat_path):
-        self.mat_path = mat_path
-        self.features = np.squeeze(self.load_dict()["Data"])
-        self.labels = self.load_dict()["labels"]
+    def __init__(self, train_mat_path, test_mat_path):
+        self.train_mat_path = train_mat_path
+        self.test_mat_path = test_mat_path
+        self.train_features = np.squeeze(self.load_dict(self.train_mat_path)["Data"])
+        self.train_labels = self.load_dict(self.train_mat_path)["labels"]
+        self.test_features = np.squeeze(self.load_dict(self.test_mat_path)["Data"])
+        self.test_labels = self.load_dict(self.test_mat_path)["labels"]
 
     # Parsing .mat file into dictionary
-    def load_dict(self):
-        return loadmat(self.mat_path)
+    def load_dict(self, mat_path):
+        return loadmat(mat_path)
 
     # Method for resizing all arrays to a single shape
     @staticmethod
@@ -30,7 +37,7 @@ class LoadDigits:
                 width_factor = desire_shape[1]/arr.shape[1]
 
             else:
-                pad_needed = (desire_shape[0] - arr.shape[1])//2
+                pad_needed = abs((desire_shape[0] - arr.shape[1])//2)
                 arr = np.pad(arr, pad_needed, constant_values=0, mode='constant')
                 width_factor = desire_shape[1] / arr.shape[1]
                 height_factor = desire_shape[0]/arr.shape[0]
@@ -38,12 +45,20 @@ class LoadDigits:
             mat = ndimage.zoom(arr, (height_factor, width_factor))
             mat[mat > 80] = 255
             mat[mat <= 80] = 0
-            final_array[i] = mat.reshape(400)
+            final_array[i] = mat.reshape(desire_shape[0]*desire_shape[1])
         return final_array
+
+    @staticmethod
+    def skimage_resize(images, desire_shape=(20, 20)):
+        array = np.zeros((images.shape[0], desire_shape[0]*desire_shape[1]))
+        for i, img in enumerate(images):
+            img = resize(img, desire_shape)
+            array[i] = img.reshape(100)
+        return array
 
     # Splitting data to train set and test set
     def train_test_split(self, features, labels, train_size=0.8):
-        features = features//255
+        features = features/255
         labels = labels
         train_index = int(features.shape[0] * train_size)
         X_train, X_test = features[:train_index], features[train_index:]
@@ -52,12 +67,12 @@ class LoadDigits:
 
     # Plot 10 images from each 10 digit classes
     @staticmethod
-    def plot_digits(arr, label):
+    def plot_digits(arr, label, images_shape=(10, 10)):
         fig, ax = plt.subplots(10, 10, figsize=(20, 20))
         for i, row in enumerate(ax):
             label_index = np.argwhere(label == i)
             for j, column in enumerate(row):
-                column.imshow(arr.reshape(-1, 20, 20)[label_index][j][0], cmap='gray')
+                column.imshow(arr.reshape(-1, images_shape[0], images_shape[1])[label_index][j][0], cmap='gray')
         plt.show()
 
     # Saving features and labels in .npy format
@@ -66,14 +81,26 @@ class LoadDigits:
         np.save(os.path.join('dataset', f'{filename}.npy'), file)
 
 
-DATASET_PATH = "dataset/Data_hoda_full.mat"
-load_digits = LoadDigits(DATASET_PATH)
-features, labels = load_digits.features, load_digits.labels
-features_array = load_digits.resize(features)
-# load_digits.plot_digits(features_array, labels)
-X_train, X_test, y_train, y_test = load_digits.train_test_split(features_array, labels)
-load_digits.save('X_train', X_train)
-load_digits.save('X_test', X_test)
-load_digits.save('y_train', y_train)
-load_digits.save('y_test', y_test)
+TRAIN_DATASET_PATH = "dataset/Data_hoda_full.mat"
+TEST_DATASET_PATH = "dataset/Test_20000.Mat"
+load_digits = LoadDigits(TRAIN_DATASET_PATH, TEST_DATASET_PATH)
+X_train, y_train = load_digits.train_features, load_digits.train_labels
+X_test, y_test = load_digits.test_features, load_digits.test_labels
 
+# Also you can use load_digits.resize() method instead. Try both of them and compare the results
+X_train = load_digits.resize(X_train)
+X_test = load_digits.resize(X_test)
+
+load_digits.plot_digits(X_train, y_train)
+load_digits.plot_digits(X_test, y_test)
+
+# In case there is a need for validation data
+# X_train, X_val, y_train, y_val = load_digits.train_test_split(X_train, y_train)
+
+# Saving arrays (if needed)
+# load_digits.save('X_train', X_train)
+# load_digits.save('X_test', X_test)
+# load_digits.save('X_val', X_val)
+# load_digits.save('y_val', y_val)
+# load_digits.save('y_train', y_train)
+# load_digits.save('y_test', y_test)
