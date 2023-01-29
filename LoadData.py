25e85d83067f1bde231957bab7fa8e04a2f5aa34
amirmahdi-alavi <alavi_amirmahdi@yahoo.com>
2023-01-29 23:48:25 +0330

    Edited read_dataset method

--- LoadData.py
+++ LoadData.py
@@ -22,25 +22,9 @@ class LoadDigits:
     def resize(array: np.array, desire_shape=(20, 20)):
         final_array = np.zeros(shape=(array.shape[0], desire_shape[0]*desire_shape[1]))
         for i, arr in enumerate(array):
-            if arr.shape[0] < desire_shape[0] and arr.shape[1] < desire_shape[1]:
-                pad_needed = (desire_shape[0]-arr.shape[0])//2
-                arr = np.pad(arr, pad_needed, constant_values=0, mode='constant')
-                height_factor = desire_shape[0] / arr.shape[0]
-                width_factor = desire_shape[1] / arr.shape[1]
-
-            elif arr.shape[1]/arr.shape[0] > 0.3:
-                height_factor = desire_shape[0]/arr.shape[0]
-                width_factor = desire_shape[1]/arr.shape[1]
-
-            else:
-                pad_needed = abs((desire_shape[0] - arr.shape[1])//2)
-                arr = np.pad(arr, pad_needed, constant_values=0, mode='constant')
-                width_factor = desire_shape[1] / arr.shape[1]
-                height_factor = desire_shape[0]/arr.shape[0]
-
+            height_factor = desire_shape[0] / arr.shape[0]
+            width_factor = desire_shape[1] / arr.shape[1]
             mat = ndimage.zoom(arr, (height_factor, width_factor))
-            mat[mat > 80] = 255
-            mat[mat <= 80] = 0
             final_array[i] = mat.reshape(desire_shape[0]*desire_shape[1])
         return final_array/255
 
@@ -53,22 +37,27 @@ class LoadDigits:
         return array/255
 
     # Splitting data to train set and test set
-    def read_dataset(self, val='remaining'):
-        train_images = np.squeeze(self.load_dict(self.train_mat_path)["Data"])
-        train_labels = self.load_dict(self.train_mat_path)["labels"]
+    def read_dataset(self, val, desire_shape):
+        train_images = self.skimage_resize(np.squeeze(self.load_dict(self.train_mat_path)["Data"]),
+                                           desire_shape=desire_shape)
+        train_labels = np.squeeze(self.load_dict(self.train_mat_path)["labels"])
         if val == 'remaining':
-            test_images = np.squeeze(self.load_dict(self.test_mat_path)["Data"])
-            test_labels = self.load_dict(self.test_mat_path)["labels"]
-            val_images = np.squeeze(self.load_dict(self.remaining_mat_path)["Data"])
-            val_labels = self.load_dict(self.remaining_mat_path)["labels"]
+            test_images = self.skimage_resize(np.squeeze(self.load_dict(self.test_mat_path)["Data"]),
+                                              desire_shape=desire_shape)
+            test_labels = np.squeeze(self.load_dict(self.test_mat_path)["labels"])
+            val_images = self.skimage_resize(np.squeeze(self.load_dict(self.remaining_mat_path)["Data"]),
+                                             desire_shape=desire_shape)
+            val_labels = np.squeeze(self.load_dict(self.remaining_mat_path)["labels"])
         elif val == 'test':
-            test_images = np.squeeze(self.load_dict(self.remaining_mat_path)["Data"])
-            test_labels = self.load_dict(self.remaining_mat_path)["labels"]
-            val_images = np.squeeze(self.load_dict(self.test_mat_path)["Data"])
-            val_labels = self.load_dict(self.test_mat_path)["labels"]
+            test_images = self.skimage_resize(np.squeeze(self.load_dict(self.remaining_mat_path)["Data"],
+                                              desire_shape=desire_shape))
+            test_labels = np.squeeze(self.load_dict(self.remaining_mat_path)["labels"])
+            val_images = self.skimage_resize(np.squeeze(self.load_dict(self.test_mat_path)["Data"]),
+                                             desire_shape=desire_shape)
+            val_labels = np.squeeze(self.load_dict(self.test_mat_path)["labels"])
         else:
             raise ValueError("Invalid name for val argument use 'remaining' or 'test'")
-        return train_images, train_labels, test_images, test_labels.ravel(), val_images.ravel(), val_labels.ravel()
+        return train_images, train_labels, test_images, test_labels, val_images, val_labels
 
     # Plot 10 images from each 10 digit classes
     @staticmethod
@@ -86,22 +75,20 @@ class LoadDigits:
         np.save(os.path.join('dataset', f'{filename}.npy'), file)
 
 
-TRAIN_DATASET_PATH = "dataset/Data_hoda_full.mat"
-TEST_DATASET_PATH = "dataset/Test_20000.Mat"
-VAL_DATASET_PATH = "dataset/Remainingset_Hoda.Mat"
+TRAIN_DATASET_PATH = "data/Trainset_Hoda.mat"
+TEST_DATASET_PATH = "data/Testset_Hoda.Mat"
+VAL_DATASET_PATH = "data/Remainingset_Hoda.Mat"
 load_digits = LoadDigits(TRAIN_DATASET_PATH, TEST_DATASET_PATH, VAL_DATASET_PATH)
 
 # pass 'remaining' or 'test' to choose your validation dataset
-img_train, label_train, img_test, label_test, img_val, label_val = load_digits.read_dataset(val='remaining')
+shape = (20, 20)
+validation_set = 'remaining'
+img_train, label_train, img_test, label_test, img_val, label_val = load_digits.read_dataset(val=validation_set,
+                                                                                            desire_shape=shape)
 
-# Also you can use load_digits.resize() method instead. Try both of them and compare the results
-img_train = load_digits.skimage_resize(img_train)
-img_test = load_digits.skimage_resize(img_test)
-img_val = load_digits.skimage_resize(img_val)
-
-load_digits.plot_digits(img_train, label_train)
-load_digits.plot_digits(img_test, label_test)
-load_digits.plot_digits(img_val, label_val)
+# load_digits.plot_digits(img_train, label_train, images_shape=shape)
+# load_digits.plot_digits(img_test, label_test, images_shape=shape)
+# load_digits.plot_digits(img_val, label_val, images_shape=shape)
 
 
 # Saving arrays (if needed)
